ae5bfe0f89b00c74b1cea8a2e90b1dc8c843b9e5
Emir Talic <emir@talic.eu>
2025-08-02 22:30:39 +0200

    added game loop

--- main.py
+++ main.py
@@ -2,10 +2,20 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    
+    while True:
+        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
+
+        pygame.display.flip() 
 
 if __name__ == "__main__":
     main()
