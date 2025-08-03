a3d8e4d7d87c6a166ceff39d4d50f05dcb50ca7d
Emir Talic <emir@talic.eu>
2025-08-03 18:56:04 +0200

    added asteriods

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    
@@ -9,11 +10,14 @@ def main():
     
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteriods = pygame.sprite.Group()
+    
 
     Player.containers = (updateable, drawable)
-
+    Asteroid.containers = (asteriods, updateable, drawable)
+    AsteroidField.containers = (updateable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+    asteriod_field = AsteroidField()
     dt = 0
     while True:
         for event in pygame.event.get():
@@ -24,6 +28,7 @@ def main():
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
+       
 
         pygame.display.flip() 
         #limit the framerate
