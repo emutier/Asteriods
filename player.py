8941db7527b45a8ce823577d07ef8577e8a09677
Emir Talic <emir@talic.eu>
2025-08-05 00:18:25 +0200

    add cooldown to shooting

--- player.py
+++ player.py
@@ -33,7 +33,7 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-        if keys[pygame.K_SPACE] and self.cooldown_time <= 0:
+        if keys[pygame.K_SPACE]:# and self.cooldown_time <= 0:
             self.shoot(dt=dt)
 
     def rotate(self, dt):
@@ -44,8 +44,11 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self,dt):
-        shot = Shot(self.position.x, self.position.y)
+        if self.cooldown_time > 0:
+            return
         
+        shot = Shot(self.position.x, self.position.y)
+        self.cooldown_time = PLAYER_SHOOT_COOLDOWN
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         shot.velocity += forward * PLAYER_SHOOT_SPEED* dt
 
