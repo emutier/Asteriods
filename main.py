262784cbf86fe9342ed9ed11fe3afea6da5ce0a7
Emir Talic <emir@talic.eu>
2025-08-05 00:06:51 +0200

    adde shots

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    
@@ -12,13 +13,15 @@ def main():
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteriods = pygame.sprite.Group()
-    
+    shots = pygame.sprite.Group()
 
     Player.containers = (updateable, drawable)
     Asteroid.containers = (asteriods, updateable, drawable)
     AsteroidField.containers = (updateable)
+    Shot.containers = (shots,updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteriod_field = AsteroidField()
+    # shot = Shot(player.position.x, player.position.y)
     dt = 0
     while True:
         for event in pygame.event.get():
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import * 
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self,x,y):
@@ -30,6 +31,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot(dt=dt)
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -37,3 +40,10 @@ class Player(CircleShape):
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self,dt):
+        shot = Shot(self.position.x, self.position.y)
+        
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        shot.velocity += forward * PLAYER_SHOOT_SPEED* dt
+
